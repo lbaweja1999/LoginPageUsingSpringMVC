f74b06245750487172bdce5f7d549f779885464b
Lakshay Baweja <lakshay.baweja@pieinfosystems.com>
2019-12-12 18:12:16 +0530

    fm

---
+++ src/main/java/com/lakshay/SecurityWebApplicationInitializer.java
@@ -0,0 +1,5 @@
+package com.lakshay;  
+import org.springframework.security.web.context.*;  
+public class SecurityWebApplicationInitializer  
+    extends AbstractSecurityWebApplicationInitializer {  
+}  
